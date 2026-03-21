1b7ea1b169f27a92222e5b3cdf3455adf2ee7f7a
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-03-21 01:32:38 -0300

    Correções para salvar Token no browser

--- webapi-django/config/settings.py
+++ webapi-django/config/settings.py
@@ -166,7 +166,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+]
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-csrftoken",
 ]
