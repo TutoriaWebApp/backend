e14ed9bd78e39df47ede7d12f2e10947509ccdc7
Rocsantos <ro12062000@gmail.com>
2026-05-02 11:33:06 -0300

    :test_tube: Adicionando testes de viewset

---
+++ webapi-django/test/views/__init__.py
@@ -0,0 +1,11 @@
+from .UsuarioViewSetTest import UsuarioViewSetTest
+from .ConquistaViewSetTest import ConquistaViewSetTest
+from .SessaoViewSetTest import SessaoViewSetTest
+from .TutorViewSetTest import TutorViewSetTest
+
+__all__ = [
+	'UsuarioViewSetTest',
+	'ConquistaViewSetTest',
+	'SessaoViewSetTest',
+	'TutorViewSetTest',
+]
