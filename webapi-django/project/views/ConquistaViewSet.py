4db642956e30da44fa8e75edf82ea7c4d715db85
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2026-09-11 21:27:33 -0300

    Conquistas já alcançadas retornam 200

--- webapi-django/project/views/ConquistaViewSet.py
+++ webapi-django/project/views/ConquistaViewSet.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from project.models import *
@@ -25,10 +26,35 @@ class ConquistaViewSet(viewsets.ReadOnlyModelViewSet):
 	tags=['Conquistas']
 )
 class consegueViewSet(viewsets.ModelViewSet):
-	queryset = consegueModel.objects.all()
-	serializer_class = consegueSerializer
-	permission_classes = [IsAuthenticated]
-	http_method_names = ['get', 'post']
+    queryset = consegueModel.objects.all()
+    serializer_class = consegueSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'post']
+
+    def create(self, request, *args, **kwargs):
+        usuario_id = request.data.get('usuarioId')
+        conquista_id = request.data.get('conquistaId')
+
+        if not usuario_id or not conquista_id:
+            return Response(
+                {"mensagem": "usuarioId e conquistaId são obrigatórios."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Busca ou cria o vínculo sem estourar o erro de UniqueTogetherValidator
+        consegue, criado = consegueModel.objects.get_or_create(
+            usuarioId_id=usuario_id,
+            conquistaId_id=conquista_id
+        )
+
+        serializer = self.get_serializer(consegue)
+        return Response(
+            {
+                "desbloqueadoAgora": criado,
+                "dados": serializer.data
+            },
+            status=status.HTTP_201_CREATED if criado else status.HTTP_200_OK
+        )
 
 @extend_schema(
 	summary="Confirma a alteração de senha",
